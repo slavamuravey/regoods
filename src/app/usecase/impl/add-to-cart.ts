ddc9bcfeb028d6c2f4a5563e70886d2388eb8d43
slavamuravey <slavamuravey@mail.ru>
2022-08-29 22:01:32 +0700

    Add search product functionality

--- src/app/usecase/impl/add-to-cart.ts
+++ src/app/usecase/impl/add-to-cart.ts
@@ -1,8 +1,9 @@
 import { createStepMessage } from "../utils";
-import { Get } from "../actions";
+import { Click, Get, SendKeys } from "../actions";
 import { SECOND } from "../../../libs/time";
 import { createDriver } from "../../../libs/selenium-webdriver";
-import { AddToCartParams, AddToCartUsecase } from "../add-to-cart";
+import { AddToCartParams, AddToCartUsecase, AddToCartUsecaseError } from "../add-to-cart";
+import { By, Key } from "selenium-webdriver";
 import type { WbUserRepository } from "../../repository/wb-user";
 import type { StepMessage } from "../step-message";
 
@@ -41,9 +42,47 @@ export class AddToCartUsecaseImpl implements AddToCartUsecase {
         await driver.manage().addCookie(cookie);
       }
 
-      await driver.get("https://www.wildberries.ru/lk");
+      await driver.get("https://www.wildberries.ru");
       await driver.sleep(SECOND);
-      yield createStepMessage(new Get("https://www.wildberries.ru/lk"), "Open profile page", await driver.takeScreenshot());
+      yield createStepMessage(new Get("https://www.wildberries.ru"), "Open main page as logged in user", await driver.takeScreenshot());
+
+      const searchInput = driver.findElement(By.id("searchInput"));
+      await searchInput.sendKeys(keyPhrase);
+      await driver.sleep(SECOND);
+      yield createStepMessage(new SendKeys(keyPhrase), "Send key phrase keys to search input", await driver.takeScreenshot());
+
+      await searchInput.sendKeys(Key.RETURN);
+      await driver.sleep(3 * SECOND);
+      yield createStepMessage(new SendKeys(keyPhrase), "Get search results", await driver.takeScreenshot());
+
+      while (true) {
+        let item = null;
+
+        try {
+          item = await driver.findElement(By.id(`c${vendorCode}`));
+        } catch {}
+
+        if (item !== null) {
+          await item.click();
+          await driver.sleep(SECOND * 2);
+          yield createStepMessage(new Click(), `Open item with vendor code "${vendorCode}"`, await driver.takeScreenshot());
+          break;
+        }
+
+        let nextPageLink = null;
+
+        try {
+          nextPageLink = await driver.findElement(By.className("pagination__next"));
+        } catch {}
+
+        if (nextPageLink === null) {
+          throw new AddToCartUsecaseError(`product with vendor code "${vendorCode}" is not found.`);
+        }
+
+        await nextPageLink.click();
+        await driver.sleep(SECOND * 2);
+        yield createStepMessage(new Click(), "Click next page link", await driver.takeScreenshot());
+      }
     } finally {
       if (quit) {
         driver.quit();
