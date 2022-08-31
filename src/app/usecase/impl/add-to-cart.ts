8f813deb83dd2d94c23e011f0f306f595d68ad39
slavamuravey <slavamuravey@mail.ru>
2022-08-31 13:50:56 +0700

    Add choose address functionality

--- src/app/usecase/impl/add-to-cart.ts
+++ src/app/usecase/impl/add-to-cart.ts
@@ -46,6 +46,30 @@ export class AddToCartUsecaseImpl implements AddToCartUsecase {
       await driver.sleep(SECOND);
       yield createStepMessage(new Get("https://www.wildberries.ru"), "Open main page as logged in user", await driver.takeScreenshot());
 
+      const addressLink = driver.findElement(By.className("simple-menu__link--address"));
+      await addressLink.click();
+      await driver.sleep(2 * SECOND);
+      yield createStepMessage(new Click(), "Click address link", await driver.takeScreenshot());
+
+      const addressInput = driver.findElement(By.css("input[class*='searchbox-input__input']"));
+      await addressInput.sendKeys(address);
+      await driver.sleep(SECOND);
+      yield createStepMessage(new SendKeys(address), "Send address to address input", await driver.takeScreenshot());
+
+      await addressInput.sendKeys(Key.RETURN);
+      await driver.sleep(3 * SECOND);
+      yield createStepMessage(new SendKeys(Key.RETURN), "Get suggested addresses", await driver.takeScreenshot());
+
+      const addressItem = driver.findElement(By.xpath(`//span[text()='${address}']`));
+      await addressItem.click();
+      await driver.sleep(2 * SECOND);
+      yield createStepMessage(new Click(), "Click address item", await driver.takeScreenshot());
+
+      const chooseAddressButton = driver.findElement(By.className("balloon-content-block-btn"));
+      await chooseAddressButton.click();
+      await driver.sleep(2 * SECOND);
+      yield createStepMessage(new Click(), "Choose address", await driver.takeScreenshot());
+
       const searchInput = driver.findElement(By.id("searchInput"));
       await searchInput.sendKeys(keyPhrase);
       await driver.sleep(SECOND);
@@ -53,7 +77,7 @@ export class AddToCartUsecaseImpl implements AddToCartUsecase {
 
       await searchInput.sendKeys(Key.RETURN);
       await driver.sleep(3 * SECOND);
-      yield createStepMessage(new SendKeys(keyPhrase), "Get search results", await driver.takeScreenshot());
+      yield createStepMessage(new SendKeys(Key.RETURN), "Get search results", await driver.takeScreenshot());
 
       while (true) {
         let item = null;
